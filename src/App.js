4ec836f6735be29194fbc5197bd2cd4cd6464e6a
Daniel Schmidt <dschmidt@mesosphere.com>
2019-05-20 16:07:44 +0200

    refactor: move stream into own file

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import React from "react";
 import Webcam from "react-webcam";
-import * as Rx from "rxjs";
-import { map, tap, switchMap, filter, catchError } from "rxjs/operators";
+import { map } from "rxjs/operators";
 import "./App.css";
-import * as posenet from "@tensorflow-models/posenet";
+
+import positionsStream from "./positionsStream";
 
 const WEBCAM_HEIGHT = 500;
 const WEBCAM_WIDTH = 500;
 
 const interval = 300;
-
-const model$ = Rx.from(posenet.load());
-
-function findPart(keypoints, name) {
-  return keypoints.find(({ part }) => part === name);
-}
 
 function calculateTilt(left, right) {
   return (
@@ -31,65 +25,27 @@ class App extends React.Component {
   setRef = webcam => {
     this.webcam = webcam;
 
-    this.image$ = Rx.interval(interval).pipe(
-      map(() => webcam.getScreenshot()),
-      filter(src => src !== null),
-      tap(imageSrc => {
-        this.image.src = imageSrc;
-      })
-    );
-    const tiltDifference$ = Rx.combineLatest(model$, this.image$).pipe(
-      switchMap(([net, imageSrc]) => {
-        // TODO: get real aspect ratio of camera
-        // TODO: try to not use DOM
-        // const imageElement = new Image(WEBCAM_WIDTH, WEBCAM_HEIGHT);
-        // imageElement.src = imageSrc;
-        const imageScaleFactor = 0.5;
-        const flipHorizontal = true;
-        const outputStride = 16;
-
-        return Rx.from(
-          net.estimateSinglePose(
-            document.getElementById("image"),
-            imageScaleFactor,
-            flipHorizontal,
-            outputStride
-          )
-        );
-      }),
-      filter(pose => pose.score > 0.3),
-      map(pose => {
-        const findThePart = findPart.bind(null, pose.keypoints);
-        const [
+    const tiltDifference$ = positionsStream(webcam, interval).pipe(
+      map(
+        ({
           leftEar,
           leftEye,
           leftShoulder,
           rightEar,
           rightEye,
           rightShoulder
-        ] = [
-          "leftEar",
-          "leftEye",
-          "leftShoulder",
-          "rightEar",
-          "rightEye",
-          "rightShoulder"
-        ].map(findThePart);
+        }) => {
+          // TODO: chek why head scales twice as fast
+          const earTilt = calculateTilt(leftEar, rightEar);
+          const eyeTilt = calculateTilt(leftEye, rightEye);
+          const headAxisTilt = (earTilt + eyeTilt) / 2; // TODO: include score
 
-        console.log(
-          ...[leftEar, leftEye, leftShoulder, rightEar, rightEye, rightShoulder]
-        );
+          const shoulderTilt = calculateTilt(leftShoulder, rightShoulder);
+          const bodyAxisTilt = shoulderTilt; // TODO: add elbows
 
-        // TODO: chek why head scales twice as fast
-        const earTilt = calculateTilt(leftEar, rightEar);
-        const eyeTilt = calculateTilt(leftEye, rightEye);
-        const headAxisTilt = (earTilt + eyeTilt) / 2; // TODO: include score
-
-        const shoulderTilt = calculateTilt(leftShoulder, rightShoulder);
-        const bodyAxisTilt = shoulderTilt; // TODO: add elbows
-
-        return { headAxisTilt, bodyAxisTilt };
-      })
+          return { headAxisTilt, bodyAxisTilt };
+        }
+      )
     );
 
     tiltDifference$.subscribe({
---
+++ src/positionsStream.js
@@ -0,0 +1,69 @@
+import * as Rx from "rxjs";
+import { map, tap, switchMap, filter } from "rxjs/operators";
+import * as posenet from "@tensorflow-models/posenet";
+
+const model$ = Rx.from(posenet.load());
+
+function findPart(keypoints, name) {
+  return keypoints.find(({ part }) => part === name);
+}
+
+export default function positionsStream(webcam, interval) {
+  const image$ = Rx.interval(interval).pipe(
+    map(() => webcam.getScreenshot()),
+    filter(src => src !== null),
+    tap(imageSrc => {
+      this.image.src = imageSrc;
+    })
+  );
+
+  return Rx.combineLatest(model$, image$).pipe(
+    switchMap(([net, _imageSrc]) => {
+      // TODO: get real aspect ratio of camera
+      // TODO: try to not use DOM
+      // const imageElement = new Image(WEBCAM_WIDTH, WEBCAM_HEIGHT);
+      // imageElement.src = imageSrc;
+      const imageScaleFactor = 0.5;
+      const flipHorizontal = true;
+      const outputStride = 16;
+
+      return Rx.from(
+        net.estimateSinglePose(
+          document.getElementById("image"),
+          imageScaleFactor,
+          flipHorizontal,
+          outputStride
+        )
+      );
+    }),
+    filter(pose => pose.score > 0.3),
+    map(pose => {
+      const findThePart = findPart.bind(null, pose.keypoints);
+
+      const [
+        leftEar,
+        leftEye,
+        leftShoulder,
+        rightEar,
+        rightEye,
+        rightShoulder
+      ] = [
+        "leftEar",
+        "leftEye",
+        "leftShoulder",
+        "rightEar",
+        "rightEye",
+        "rightShoulder"
+      ].map(findThePart);
+
+      return {
+        leftEar,
+        leftEye,
+        leftShoulder,
+        rightEar,
+        rightEye,
+        rightShoulder
+      };
+    })
+  );
+}
